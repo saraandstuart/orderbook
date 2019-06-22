6c1b7cbf6dc41648b73437a7d33081f1c2e79fa5
Stuart Shannon <shannoncode@yahoo.com>
2019-06-22 13:06:41 +0100

    ability to access H2 Database over Web and Tcp clients

--- src/main/java/com/creditsuisse/orderbook/config/LoadDatabase.java
+++ src/main/java/com/creditsuisse/orderbook/config/LoadDatabase.java
@@ -1,11 +1,15 @@
 package com.creditsuisse.orderbook.config;
 
-import com.creditsuisse.orderbook.persist.OrderBookRepository;
+import com.creditsuisse.orderbook.model.Execution;
+import com.creditsuisse.orderbook.model.Order;
 import com.creditsuisse.orderbook.model.OrderBook;
+import com.creditsuisse.orderbook.persist.OrderBookRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 /**
  * @author Stuart Shannon
@@ -17,7 +21,19 @@ public class LoadDatabase
     @Bean
     CommandLineRunner initDatabase(OrderBookRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new OrderBook("finiancialInstrumentId-1")));
+            OrderBook orderBook = new OrderBook("finiancialInstrumentId-1");
+
+            Order order = new Order();
+            Execution execution = new Execution();
+
+//            order.setOrderBook(orderBook);
+//            execution.setOrderBook(orderBook);
+
+            orderBook.setOrders(Arrays.asList(order));
+            orderBook.setExecutions(Arrays.asList(execution));
+
+
+            log.info("Preloading " + repository.save(orderBook));
             log.info("Preloading " + repository.save(new OrderBook("finiancialInstrumentId-2")));
         };
     }
